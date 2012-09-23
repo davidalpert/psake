03bc525eb1ed3f8ea269ba7e27ee2da52579ebe9
David Alpert <david@spinthemoose.com>
2012-09-22 22:38:04 -0500

    basic mstest command-line execution

---
+++ TestProject1/default.ps1
@@ -0,0 +1,76 @@
+properties {
+	$base_dir = resolve-path .
+	$configuration = "Debug"
+	$environment = "QA"
+}
+
+task default -depends Clean, Build, Test
+
+task Clean {
+	$folders_to_clean = @(
+		".\TestProject1\bin",
+		".\TestProject1\obj"
+	) 
+
+	$folders_to_clean | foreach-object ($_) {
+		if (Test-Path $_) { Remove-Item -force -recurse $_ }
+	}
+}
+
+task Build {
+	$v4_net_version = (ls "$env:windir\Microsoft.NET\Framework\v4.0*").Name
+
+	Write-Host "Using $v4_net_version"
+	exec { &"C:\Windows\Microsoft.NET\Framework\$v4_net_version\MSBuild.exe" "$base_dir\TestProject1\TestProject1.csproj" }
+}
+
+task Test {
+	$test_assemblies = (Get-ChildItem "." -recurse -filter "*.dll") |
+														? { $_.FullName -match "\\bin\\$configuration\\Test.*?\d\.dll" }
+								
+	$test_assemblies | foreach-object ($_) {
+		Invoke-MSTest $_.FullName
+	}
+}
+
+function Invoke-MSTest
+{
+  [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)] [string]$TestDll = $null,
+        [Parameter(Position=1,Mandatory=0)] [string]$ResultTrx = $null
+        )
+				
+		if ($ResultTrx -eq $null -or $resultTrx -eq "") {
+			$ResultTrx = "$TestDll.trx"
+		}
+
+		$mstest = "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\mstest.exe"
+        
+    Write-Host "Running Tests:"
+		Write-Host "- Located at:" $TestDll 
+		Write-Host "-  Output to:" $ResultTrx
+    
+    if ( Test-Path $ResultTrx )       
+    {
+        Write-Host "Found" $ResultTrx "it will be deleted prior to running the test"
+        Remove-Item $ResultTrx
+    }       
+
+    $result = & $mstest /testcontainer:$TestDll /resultsfile:$ResultTrx
+
+		Write-Host ""
+    
+    foreach( $line in $result )
+    {
+        if ( $line -ne $null )
+        {
+            if ($line.StartsWith("Failed") -eq $true -or $line -match "^\d+\/\d+ test")
+            {
+                $line
+            }
+        }
+    }
+
+		Write-Host ""
+}
